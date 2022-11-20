578d30718578227f3ae9808ad4409c5e4c00aaf5
faroukbas <imajisetting@gmail.com>
2022-11-20 11:21:49 +0700

    cek 3

--- Main.py
+++ Main.py
@@ -3,6 +3,10 @@ test demo file python pertama
 semoga lancar
 """
 
-print ('hello')
-print ('my name is efbee')
-print ('cek 2')
+# squensial
+
+print('ibu berkata, "pergilah ke toko"')
+print('budi menjawab, "baik apa yang saya lakukan di toko ?"')
+print('ibu berkata, "beli satu botol susu, jika ada telor, belilah 6"')
+print('maka budi berangkat ke toko')
+print('dan mulai berbelanja')
